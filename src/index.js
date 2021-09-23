f478c2df51aab84d8e94514827c13ebd1e5cf62a
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2021-09-23 03:36:53 +0300

    improvements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-require('ace-css/css/ace.css');
-require('font-awesome/css/font-awesome.css');
-require('./styles.css');
-require('./Digitaldream.ttf');
+require("ace-css/css/ace.css");
+require("font-awesome/css/font-awesome.css");
+require("./styles.css");
+require("./YourFavFontHere.ttf");
 
-require('./index.html');
+require("./index.html");
 
-var Elm = require('./App.elm');
-var mountNode = document.getElementById('main');
+var Elm = require("./App.elm");
+var mountNode = document.getElementById("main");
 
 var app = Elm.App.embed(mountNode);
